ede4016a866fa4efe89a7f2a9682bc9f219d8d4a
Counter FX <christoperjoel1@gmail.com>
2025-05-31 16:11:42 +0700

    UI selesai semua

--- JS/Index.js
+++ JS/Index.js
@@ -1,10 +1,21 @@
-$(document).on('click', function (e) {
-    const $menu = $('#dropdownMenu');
-    const $toggleButton = $('.group');
+$(document).ready(function () {
+    $("#hamburgerBtn").click(function (e) {
+        e.stopPropagation();
+        $("#dropdownMenu").toggleClass("translate-x-0 translate-x-full");
+    });
 
-    if ($toggleButton.has(e.target).length > 0 || $toggleButton.is(e.target)) {
-        $menu.toggleClass('translate-x-0');
-    } else if (!$menu.is(e.target) && $menu.has(e.target).length === 0) {
-        $menu.removeClass('translate-x-0');
+    // Click outside to close
+    $(document).click(function (e) {
+        if (!$(e.target).closest("#dropdownMenu").length && !$(e.target).is("#hamburgerBtn")) {
+            $("#dropdownMenu").addClass("translate-x-full").removeClass("translate-x-0");
+        }
+    });
+
+    window.showDialog = function () {
+        $("#modal").removeClass("hidden");
+    }
+
+    window.closeDialog = function () {
+        $("#modal").addClass("hidden");
     }
 });
